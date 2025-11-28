71098c4932f77fd06106308cd5c4a1622043f374
JulianMohamed12 <julianmmohamed@gmail.com>
2025-11-27 20:06:06 -0800

    -browseActivityV2

--- app/src/main/java/com/example/project/Listing.java
+++ app/src/main/java/com/example/project/Listing.java
@@ -10,15 +10,79 @@ public class Listing {
     String startDateTime;
     String endDateTime;
 
-    public Listing(String title, String requesterName, String helperName, String category, String desc, String startDateTime, String endDateTime) {
+    public Listing(String title, String requesterName, String category, String desc, String startDateTime, String endDateTime) {
         this.title = title;
         this.requesterName = requesterName;
-        this.helperName = helperName;
+        this.helperName = null;
         this.category = category;
         this.desc = desc;
         this.startDateTime = startDateTime;
         this.endDateTime = endDateTime;
+        this.isComplete = false;
 
-        boolean isComplete = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRequesterName() {
+        return requesterName;
+    }
+
+    public void setRequesterName(String requesterName) {
+        this.requesterName = requesterName;
+    }
+
+    public String getHelperName() {
+        return helperName;
+    }
+
+    public void setHelperName(String helperName) {
+        this.helperName = helperName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+
+    public String getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(String startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public String getEndDateTime() {
+        return endDateTime;
+    }
+
+    public void setEndDateTime(String endDateTime) {
+        this.endDateTime = endDateTime;
     }
 }
---
+++ app/src/main/java/com/example/project/User.java
@@ -0,0 +1,7 @@
+package com.example.project;
+
+public class User {
+    static int idNum;
+    String userId;
+    String name;
+}
