3565fa82e508d57b96725053eb866b22fc285753
turbo-leg <turbileg@student.ubc.ca>
2025-12-02 11:13:06 -0800

    Add hamburger menu navigation to Review Page

--- app/src/main/java/com/example/project/BrowseHelpRequestsActivity.java
+++ app/src/main/java/com/example/project/BrowseHelpRequestsActivity.java
@@ -1,16 +1,23 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,6 +36,10 @@ public class BrowseHelpRequestsActivity extends AppCompatActivity {
                                         {"Babysitting for Wednesday","Jim Rolland","Babysitting"},
                                         {"Dog Walking for One Hour", "Cathy Franks","Pet Care"}};
 
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+    private ImageButton hamButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +50,35 @@ public class BrowseHelpRequestsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navView = findViewById(R.id.nav_view);
+        hamButton = findViewById(R.id.hamButton);
+
+        hamButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.hamReview) {
+                    Intent intent = new Intent(BrowseHelpRequestsActivity.this, ReviewActivity.class);
+                    startActivity(intent);
+                } else if (id == R.id.hamStats) {
+                    Intent intent = new Intent(BrowseHelpRequestsActivity.this, ViewStatistics.class);
+                    startActivity(intent);
+                }
+                // Add other menu items here as needed
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
         root = FirebaseDatabase.getInstance("https://neighborhood-help-exchange-default-rtdb.firebaseio.com/").getReference();
         intializeList();
         lvCategory = findViewById(R.id.listView);
