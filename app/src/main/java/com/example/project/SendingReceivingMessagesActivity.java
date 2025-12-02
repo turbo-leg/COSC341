fb4d678a2287d9c8d0dff533ddd3062b7d9d49b9
cohenkucher <cohenkucher@gmail.com>
2025-12-02 15:06:40 -0800

    fixing some UI stuff

--- app/src/main/java/com/example/project/SendingReceivingMessagesActivity.java
+++ app/src/main/java/com/example/project/SendingReceivingMessagesActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -30,7 +31,7 @@ public class SendingReceivingMessagesActivity extends AppCompatActivity {
         Bundle bundle = intent.getExtras();
 
         Button send = findViewById(R.id.sendButton);
-        Button back = findViewById(R.id.backButton);
+        ImageButton back = findViewById(R.id.backButton);
         TextView nameView = findViewById(R.id.textView);
         String name = bundle.getString("KEY");
         nameView.setText(name);
